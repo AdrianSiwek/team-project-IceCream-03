a06a9411df30ac0242e3526c1c1e472adfbfb310
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-07-26 21:29:50 +0000

    Deploying to gh-pages from @ AdrianSiwek/team-project-IceCream-03@8eb1bd45e967f03a14b9ba404974252bee5a8c4d 🚀

--- src.6713f7f2.js
+++ src.6713f7f2.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-IceCream-03/src.57b7efe9.js.map
+//# sourceMappingURL=/team-project-IceCream-03/src.6713f7f2.js.map
